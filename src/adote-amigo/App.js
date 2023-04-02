0f3b143c9d2b09850c0e99e9f13931568740d495
AmandaCacholi <amanda.cacholi@outlook.com>
2023-04-02 13:46:42 -0300

    feat: criar restante dos templates

--- src/adote-amigo/App.js
+++ src/adote-amigo/App.js
@@ -1,4 +1,7 @@
 import LoginForm from "./components/login-form";
+import EditInformationForm from "./components/edit-information-form";
+import HomeScreen from "./components/main-screen";
+import RegisterForm from "./components/register-form";
 
 export default function App() {
   return <LoginForm />;
---
+++ src/adote-amigo/components/edit-information-form.jsx
@@ -0,0 +1,47 @@
+import { View, StyleSheet } from "react-native";
+import { Button, TextInput, DefaultTheme } from "react-native-paper";
+import Header from "./header";
+
+const EditInformationForm = () => {
+  return (
+    <View>
+      <Header title="Editar informações" screenType="editing" />
+      <View style={styles.container}>
+        <View style={styles.containerForm}>
+          <TextInput label="Nome" />
+          <TextInput label="Email" keyboardType="email-address" />
+          <TextInput label="Telefone" />
+          <TextInput label="Data de nascimento" />
+          <TextInput label="CPF" />
+          <TextInput secureTextEntry={true} label="Senha" type="password" />
+          <Button mode="contained">Salvar</Button>
+        </View>
+        <View style={styles.deleteButtonContainer}>
+          <Button
+            buttonColor={DefaultTheme.colors.errorContainer}
+            textColor={DefaultTheme.colors.error}
+            mode="contained-tonal"
+          >
+            Excluir conta
+          </Button>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default EditInformationForm;
+
+const styles = StyleSheet.create({
+  containerForm: {
+    gap: 25,
+  },
+  container: {
+    marginHorizontal: 20,
+    marginTop: 50,
+  },
+  deleteButtonContainer: {
+    alignItems: "center",
+    marginTop: 135,
+  },
+});
