63594ba8a8acc70c727083c5eda5fb3740851496
Marcusvinimf <marcusvinimf@gmail.com>
2023-04-24 20:59:25 -0300

    implementacao de estrutura backend

---
+++ src/api/src/main/java/com/adoteanimais/api/AdoteAnimaisApplication.java
@@ -0,0 +1,13 @@
+package com.adoteanimais.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteAnimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoteAnimaisApplication.class, args);
+	}
+
+}
---
+++ src/api/src/test/java/com/adoteanimais/api/AdoteAnimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adoteanimais.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteAnimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
