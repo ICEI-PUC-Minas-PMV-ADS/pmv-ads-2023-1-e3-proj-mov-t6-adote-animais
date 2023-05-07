ca2b75c0162dc3a26cdef77225e6861f549c3edc
Gxavier <103543979+Gxavier7@users.noreply.github.com>
2023-05-07 00:39:49 -0300

    Update edit-information-form.jsx

--- src/adote-amigo/components/edit-information-form.jsx
+++ src/adote-amigo/components/edit-information-form.jsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { Button, TextInput, DefaultTheme } from "react-native-paper";
 
-const EditInformationForm = () => {
+const EditInformationForm = () => {  
+
+  const [form, setForm] = useState({});
+
+  function confirmInputs() {
+    if (!form.nome || !form.email || !form.telefone || !form.nascimento || !form.CPF || !form.password){
+      alert("Preencha todos os campos")
+    }else{
+      console.log("Save")
+    }
+
+    
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.containerForm}>
-        <TextInput label="Nome"/> 
-        <TextInput label="Email" keyboardType="email-address"/>
-        <TextInput label="Telefone"/>
-        <TextInput label="Data de nascimento"/>
-        <TextInput label="CPF"/>
-        <TextInput secureTextEntry={true} label="Senha" type="password"/>
-        <Button mode="contained" onPress={() => console.log("pressionado")}>
+        <TextInput label="Nome" onChangeText={(nome) => {
+          setForm((state) => ({ ...state, nome }));
+        }}/> 
+        <TextInput label="Email" keyboardType="email-address" onChangeText={(email) => {
+          setForm((state) => ({ ...state, email }));
+        }}/>
+        <TextInput label="Telefone" onChangeText={(telefone) => {
+          setForm((state) => ({ ...state, telefone }));
+        }}/>
+        <TextInput label="Data de nascimento" onChangeText={(nascimento) => {
+          setForm((state) => ({ ...state, nascimento }));
+        }}/>
+        <TextInput label="CPF" onChangeText={(CPF) => {
+          setForm((state) => ({ ...state, CPF }));
+        }}/>
+        <TextInput secureTextEntry={true} label="Senha" type="password" onChangeText={(password) => {
+          setForm((state) => ({ ...state, password }));
+        }}/>
+        <Button mode="contained" onPress={() => confirmInputs()}>
           Salvar
         </Button>
         <Button
